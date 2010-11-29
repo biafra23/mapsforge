6c0777a058f926fdf34e0563e4e420d774c01d90
Dirk Jaeckel <cmips@dirk.jaeckel.name>
2010-11-29 21:03:12 +0100

    zoom change notifier

---
+++ src/org/mapsforge/android/maps/ZoomChangeListener.java
@@ -0,0 +1,11 @@
+package org.mapsforge.android.maps;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: biafra
+ * Date: 11/28/10
+ * Time: 11:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ZoomChangeListener {
+}
